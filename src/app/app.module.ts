90f79945c1ab85033101c5d652c93d03ed5912d8
kishan2019 <kishandesc@gmail.com>
2020-03-25 23:08:11 +0530

    added login page and routing also created admin and user component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+ import {AdminComponent } from './admin/admin.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    AdminComponent
   ],
   imports: [
   BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+    imports: [
+        MatButtonModule, 
+        MatCardModule, 
+        MatDialogModule, 
+        MatInputModule,
+        MatToolbarModule, 
+        MatMenuModule, 
+        MatIconModule, 
+        
+    ],
+
+    exports: [
+        MatButtonModule, 
+        MatCardModule, 
+        MatDialogModule, 
+        MatInputModule, 
+        MatToolbarModule, 
+        MatMenuModule, 
+        MatIconModule, 
+        ]
 })
 
 export class MaterialModule {
