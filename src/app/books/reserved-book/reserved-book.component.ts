879367b8010d9ab931631498ce32acb237f527f8
kishan2019 <kishandesc@gmail.com>
2020-04-02 14:59:34 +0530

    added some logic to show only current user reserved book

--- src/app/books/reserved-book/reserved-book.component.ts
+++ src/app/books/reserved-book/reserved-book.component.ts
@@ -1,3 +1,6 @@
+import { LoginService } from './../../login/login.service';
+import { User } from './../user.model';
+import { Router } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Subscription} from 'rxjs';
 import { Book } from '../book.model';
@@ -9,10 +12,18 @@ import { BooksService } from '../books.service';
   styleUrls: ['./reserved-book.component.css']
 })
 export class ReservedBookComponent implements OnInit, OnDestroy {
-  books: Book[] = [];
-  private booksSub: Subscription;
 
-  constructor(public booksService: BooksService){ }
+  books: Book[] = [];
+  users: User[] = [];
+
+  private booksSub: Subscription;
+  private usersSub: Subscription;
+
+  filterSearch: string;
+  toggle: boolean = false;
+  username: string;
+
+  constructor(public booksService: BooksService, private data: LoginService, public router: Router){ }
 
   ngOnInit(){
     this.booksService.getBooks();
@@ -20,10 +31,18 @@ export class ReservedBookComponent implements OnInit, OnDestroy {
     .subscribe((books: Book[]) => {
       this.books = books;
     });
+
+    this.booksService.getUsers();
+    this.usersSub = this.booksService.getUserUpdateListener()
+    .subscribe((users: User[]) => {
+      this.users = users;
+    });
+    this.data.currentUser.subscribe(username => this.username = username)
   }
 
   ngOnDestroy(){
     this.booksSub.unsubscribe(); 
+    this.usersSub.unsubscribe();
   }
 
 }
